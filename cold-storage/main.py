b59f57f8fb2359ba6166ee4ee1d0003796789eef
vlwk <victorlohwk@gmail.com>
2018-11-07 01:24:19 +0800

    ipynb

--- cold-storage/main.py
+++ cold-storage/main.py
@@ -38,7 +38,7 @@ def dfs(node):
         dfs(nd)
 
 content = read(BASE_URL)
-tmp = content.find_all("li", {"class": "col-sm-6 col-md-3 menu-promo"})
+tmp = content.find_all("li", {"class": "co6l-sm-6 col-md-3 menu-promo"})
 for i in tmp[:2]:
     j = i.find_all('a')
     for k in j:
@@ -47,5 +47,20 @@ for i in tmp[:2]:
 for x in categories:
     dfs(x)
 
-for x in children:
-    print(x)
+for y in children:
+    ct = read(BASE_URL + y)
+    pdts = content.find_all('li',{"class":"col-xs-6 col-sm-4 col-md-3 col-lg-2 open-product-detail algolia-click"})
+    for x in pdts2:
+        a = x.find('a',{"class":"search product-quick-view"})
+        b = x.find('a',{"class":"product-link"})
+        c = x.find_all('div',{"class":"content_price"})[0]
+        d = c.find('div')
+        e = x.find_all('div',{"class":"promo-wrapper"})[0]
+        f = e.find_all('span')
+        print(a['data-product-size'])
+        print("url: "+b['href'])
+        print("name: "+b.text)
+        print("price: "+d.text)
+        if(len(f)>0):
+            print("promotion: "+f[0].text)
+        print("\n\n")
--- cold-storage/test.py
+++ cold-storage/test.py
@@ -10,6 +10,20 @@ def read(url):
 BASE_URL = "https://coldstorage.com.sg/"
 
 content = read(BASE_URL + "fruits-vegetables/fresh-fruits/apples-pears")
-pdts = content.find_all('a', {"class": "product-link"})
-for i in pdts:
-    print(i)
+
+pdts2=content.find_all('li',{"class":"col-xs-6 col-sm-4 col-md-3 col-lg-2 open-product-detail algolia-click"})
+
+for x in pdts2:
+    a = x.find('a',{"class":"search product-quick-view"})
+    b = x.find('a',{"class":"product-link"})
+    c = x.find_all('div',{"class":"content_price"})[0]
+    d = c.find('div')
+    e = x.find_all('div',{"class":"promo-wrapper"})[0]
+    f = e.find_all('span')
+    print(a['data-product-size'])
+    print("url: "+b['href'])
+    print("name: "+b.text)
+    print("price: "+d.text)
+    if(len(f)>0):
+        print("promotion: "+f[0].text)
+    print("\n\n")
